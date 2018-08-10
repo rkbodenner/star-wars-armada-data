8634f36a740b2a1e6c71c75026385785862712c1
jmthompson2015 <softwaredev@jeffreythompson.net>
2018-08-10 17:25:34 -0600

    Initial version.

---
+++ data/squadron-card.js
@@ -0,0 +1,54 @@
+[
+   {
+      "title": "\"Howlrunner\"",
+      "subtitle": "TIE Fighter Squadron",
+      "unique": true,
+      "faction": "Galactic Empire",
+      "speed": 4,
+      "hull": 3,
+      "squadron-attack": [0, 3, 0], // red blue black
+      "attack": [0, 1, 0], // red blue black
+      "text": "While another friendly squadron with Swarm at distance 1 is attacking a squadron, it may add 1 blue die to its attack pool.",
+      "keywords": ["Swarm"],
+      "defense-tokens": ["Brace", "Scatter"],
+      "points": 16,
+      "image": "squadron-card/howlrunner.png"
+   },
+   {
+      "title": "Luke Skywalker",
+      "subtitle": "X-wing Squadron",
+      "unique": true,
+      "faction": "Rebel Alliance",
+      "speed": 3,
+      "hull": 5,
+      "squadron-attack": [0, 4, 0], // red blue black
+      "attack": [0, 0, 1], // red blue black
+      "text": "While attacking a ship, treat the defender as having no shields.",
+      "keywords": ["Bomber", "Escort"],
+      "defense-tokens": ["Brace", "Brace"],
+      "points": 20,
+      "image": "squadron-card/luke-skywalker.png"
+   },
+   {
+      "title": "TIE Fighter Squadron",
+      "faction": "Galactic Empire",
+      "speed": 4,
+      "hull": 3,
+      "squadron-attack": [0, 3, 0], // red blue black
+      "attack": [0, 1, 0], // red blue black
+      "keywords": ["Swarm"],
+      "points": 8,
+      "image": "squadron-card/tie-fighter-squadron.png"
+   },
+   {
+      "title": "X-wing Squadron",
+      "faction": "Rebel Alliance",
+      "speed": 3,
+      "hull": 5,
+      "squadron-attack": [0, 4, 0], // red blue black
+      "attack": [1, 0, 0], // red blue black
+      "keywords": ["Bomber", "Escort"],
+      "points": 13,
+      "image": "squadron-card/x-wing-squadron.png"
+   }
+]
