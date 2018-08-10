985ae2ed331101cff615adff12c6e31bd69959f2
jmthompson2015 <softwaredev@jeffreythompson.net>
2018-08-10 13:46:29 -0600

    Initial version.

---
+++ data/objective-card.js
@@ -0,0 +1,122 @@
+[
+   {
+      "title": "Advanced Gunnery",
+      "category": "Assault",
+      "setup": "Setup: After deploying fleets, each player chooses 1 of his ships to be an objective ship, starting with the first player.",
+      "special-rule": "Special Rule: The first player's objective ship may perform each of its attacks from the same hull zone. It cannot target the same hull zone or squadron more than once each round with that hull zone.\nThe second player's objective ship may perform each of its attacks from the same hull zone, and it may do so against the same target.",
+      "end-of-round": "",
+      "end-of-game": "End of Game: The fleet point cost of a destroyed objective ship is doubled. Do not double the cost of its upgrade cards.",
+      "points": 0,
+      "image": "objective-card/advanced-gunnery.png"
+   },
+   {
+      "title": "Contested Outpost",
+      "category": "Defense",
+      "setup": "Setup: Place obstacles as normal, excluding the station. Then the second player places the station in the setup area beyond distance 1 of all obstacles and beyond distance 5 of both players' edges.",
+      "special-rule": "Special Rule: The station does not obstruct attacks and does not have the ability to discard damage cards or recover hull points.",
+      "end-of-round": "End of Round: Each player sums the command values of his ships at distance 1 of the station. The player with the highest total gains 1 victory token.",
+      "end-of-game": "",
+      "points": 20,
+      "image": "objective-card/contested-outpost.png"
+   },
+   {
+      "title": "Dangerous Territory",
+      "category": "Navigation",
+      "setup": "Setup: Obstacles must be placed in the setup area beyond distance 5 of both players' edges. After placing obstacles, place 1 objective token on each obstacle.",
+      "special-rule": "Special Rule: When a ship overlaps an obstacle, the ship's owner may remove the objective token on that obstacle to gain 1 victory token.\nWhen one of the second player's ships overlaps an asteroid field or debris field, that obstacle has no effect.",
+      "end-of-round": "",
+      "end-of-game": "",
+      "points": 15,
+      "image": "objective-card/dangerous-territory.png"
+   },
+   {
+      "title": "Fire Lanes",
+      "category": "Defense",
+      "setup": "Setup: After placing obstacles, the second player places 3 objective tokens in the setup area beyond distance 4 of both players' edges. Then the first player may move each objective token to within distance 1-2 of its current position.",
+      "special-rule": "",
+      "end-of-round": "End of Round: Each player gains 1 victory token for each objective he controls. To determine control of each token, players measure attack range and line of sight from each of their ships' hull zones as if performing attacks with battery armament targeting that objective token. The player with the highest total number of dice in his combined attack pool controls that token. If a player's ship or squadron overlaps an objective token, his opponent controls that token; if both players' ships or squadrons overlap the same token, neither player controls it.",
+      "end-of-game": "",
+      "points": 15,
+      "image": "objective-card/fire-lanes.png"
+   },
+   {
+      "title": "Fleet Ambush",
+      "category": "Defense",
+      "setup": "Setup: The portion of the setup area that is beyond distance 5 of any edge of the setup area is the Ambush Zone. Players mark the corners of the ambush zone with objective tokens.\nPlayers take turns deploying fleets as normal but must deploy all ships before deploying any squadrons. The first player must deploy ships within the ambush zone on his odd-numbered deployment turn, starting with his first turn. He cannot deploy ships or squadrons overlapping obstacles in the ambush zone. After setup is complete, he removes all objective tokens from the play area.",
+      "special-rule": "",
+      "end-of-round": "",
+      "end-of-game": "",
+      "points": 0,
+      "image": "objective-card/fleet-ambush.png"
+   },
+   {
+      "title": "Hyperspace Assault",
+      "category": "Defense",
+      "setup": "Setup: Before deploying fleets, the second player sets aside 1 of his small or medium ships and up to 3 of his squadrons; he does not deploy them during setup. Then he places 3 objective tokens in the play area beyond distance 3 of both players' edges.",
+      "special-rule": "Special Rule: At the start of any round after the first round, the second player can deploy the ship and squadrons that he set aside at distance 1 of 1 objective token. Then remove all objective tokens. The ship can be deployed overlapping squadrons; the first player places those squadrons as though the ship had overlapped them while executing a maneuver.\nIf the second player does not deploy, he may move each objective token to within distance 1 of its current position.",
+      "end-of-round": "",
+      "end-of-game": "",
+      "points": 0,
+      "image": "objective-card/hyperspace-assault.png"
+   },
+   {
+      "title": "Intel Sweep",
+      "category": "Navigation",
+      "setup": "Setup: After placing obstacles the players alternate placing a total of 5 objective tokens in the setup area, starting with the second player. Each token must be placed in the play area beyond distance 5 of both players' edges and beyond distance 3 of all other objective tokens. Then each player chooses one of his ships to be an objective ship, starting with the first player.",
+      "special-rule": "Special Rule: When a player's objective ship reveals a command dial, that player may choose 1 objective token at a distance of 1 of that ship and remove it from the play area to gain 1 victory token.",
+      "end-of-round": "",
+      "end-of-game": "End of Game: If a player has more victory tokens than his opponent, increase his final score by 75.",
+      "points": 0,
+      "image": "objective-card/intel-sweep.png"
+   },
+   {
+      "title": "Minefields",
+      "category": "Navigation",
+      "setup": "Setup: The second player places all obstacles. He can place them anywhere in the setup area (even in deployment zones) and must place them beyond distance 5 of each other. Then he places 6 objective tokens. Each objective token must be placed at distance 1 of an obstacle and beyond distance 1 of all other objective tokens.",
+      "special-rule": "Special Rule: If a ship ends its movement at distance 1 of an objective token, remove that objective token from the play area and roll 2 blue dice. That ship is dealt 1 facedown damage card for each [Hit] or [Critical Hit] icon rolled. If there is at least 1 [Critical Hit] icon, deal the first damage card faceup.",
+      "end-of-round": "",
+      "end-of-game": "",
+      "points": 0,
+      "image": "objective-card/minefields.png"
+   },
+   {
+      "title": "Most Wanted",
+      "category": "Assault",
+      "setup": "Setup: After deploying fleets, the second player chooses 1 of his ships and 1 of the first player's ships to be the objective ships.",
+      "special-rule": "Special Rule: While attacking an objective ship, the attacker may add 1 die of any color that is already in its attack pool to its attack pool.",
+      "end-of-round": "",
+      "end-of-game": "End of Game: The fleet point cost of a destroyed objective ship is doubled. Do not double the cost of its upgrade cards.",
+      "points": 0,
+      "image": "objective-card/most-wanted.png"
+   },
+   {
+      "title": "Opening Salvo",
+      "category": "Assault",
+      "setup": "Setup: After deploying fleets, assign 1 objective token to each ship.",
+      "special-rule": "Special Rule: The first time a ship performs an attack against another ship, discard the attacker's objective token. If the attacker belongs to the first player, he adds 2 red dice to the attack pool. If the attacker belongs to the second player, he adds 2 dice, each of any color, to the attack pool.",
+      "end-of-round": "",
+      "end-of-game": "End of Game: Each player increases his final score by half the fleet point cost of each enemy ship in the play area that has at least 1 damage card, rounded up.",
+      "points": 0,
+      "image": "objective-card/opening-salvo.png"
+   },
+   {
+      "title": "Precision Strike",
+      "category": "Assault",
+      "setup": "Setup: After deploying fleets, each of the second player's ships gains a [Concentrate Fire] token.",
+      "special-rule": "Special Rule: When a squadron with Bomber or a ship is attacking, it may spend 1 die with a [Hit] icon to flip 1 random facedown damage card on the defender faceup.\nAfter a squadron with Bomber or a ship performs an attack, its owner gains 1 victory token for each damage card that was dealt faceup or flipped faceup during that attack.",
+      "end-of-round": "",
+      "end-of-game": "",
+      "points": 15,
+      "image": "objective-card/precision-strike.png"
+   },
+   {
+      "title": "Superior Positions",
+      "category": "Navigation",
+      "setup": "Setup: The first player must deploy all of his ships and squadrons before the second player.",
+      "special-rule": "Special Rule: After a ship or squadron performs an attack against the rear hull zone of another ship, if the defender suffered at least 1 damage, the attacker's owner gains 1 victory token.",
+      "end-of-round": "",
+      "end-of-game": "",
+      "points": 15,
+      "image": "objective-card/superior-positions.png"
+   }
+]
